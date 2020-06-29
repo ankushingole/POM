2d8815f1d779ea9e0b0f56e8d75353009b11ae83
ankushingole <56147964+ankushingole@users.noreply.github.com>
2020-06-29 17:24:28 +0530

    updated POM to run with JEnkins

--- run.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=D:\Workspace\POM_PageFactory_OrangeHRM
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
